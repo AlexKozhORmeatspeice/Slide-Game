4425c662a8b34c241cc90d6e32f5b0b9b65f873e
Alex Kozh <rabbitdioa@mail.ru>
2021-03-12 15:08:44 +0500

    Added cam tracking

--- Assets/Scripts/CamerS/CameraMovement.cs
+++ Assets/Scripts/CamerS/CameraMovement.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    private GameObject _target;
+    [SerializeField][Range(1.0f,5.0f)]  private float _speedOfMove = 1.0f;
+
+    private Vector3 _offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _target = FindObjectOfType<Character>().gameObject;
+        _offset = _target.transform.position - transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        float targAngle = _target.transform.eulerAngles.y;
         
+        Quaternion rotation = Quaternion.Euler(0, targAngle, 0);
+
+        Vector3 pos = _target.transform.position - (rotation * _offset);
+        pos.y += 1.0f;
+        transform.position = Vector3.Lerp(transform.position,pos,Time.deltaTime * _speedOfMove);
+        
+        transform.LookAt(_target.transform);
     }
 }
