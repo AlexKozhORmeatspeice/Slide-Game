d36bc79eec7cab192a76c0dceb4a9a49cbc7669b
Alex Kozh <rabbitdioa@mail.ru>
2021-03-15 00:20:08 +0500

    Added tapes

--- Assets/Scripts/AtackScripts/Attack.cs
+++ Assets/Scripts/AtackScripts/Attack.cs
@@ -1,30 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Attack : MonoBehaviour
+public class Attack : MonoBehaviour, IPointerDownHandler
 {
+    private Vector3 origin;
+    
     [SerializeField] private GameObject _projectilePref;
     private Character _character;
 
     void Start()
     {
+        origin = Vector3.zero;
         _character = FindObjectOfType<Character>();
     }
     
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        if (Input.GetMouseButtonDown(0) && _character.UnderPressure)
+        if (origin != Vector3.zero && _character.UnderPressure)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(origin);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
                 GameObject proj = Pooler.Instance.SpawnPoolObject("Bullet", Camera.main.transform.position, Quaternion.identity);
                 proj.GetComponent<BulletConrtoller>().Target = hit.point;
             }
+
+            origin = Vector3.zero;
         }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        origin = eventData.position;
     }
 }
 
